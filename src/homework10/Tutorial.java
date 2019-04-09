445b4e5bfd6cd4a859a90321111c2aebb355e46b
hbendekgey <henry.bendekgey@pomona.edu>
2019-04-08 20:58:22 -0700

    removed executables, added README, added Tutorial

---
+++ src/homework10/Tutorial.java
@@ -0,0 +1,9 @@
+package homework10;
+
+import edu.stanford.math.plex4.api.Plex4;
+
+public class Tutorial {
+	public static void main(String[] args) {
+		System.out.println(Plex4.createExplicitSimplexStream());
+	}
+}
